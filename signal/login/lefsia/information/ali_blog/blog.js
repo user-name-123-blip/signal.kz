4a1a9ea18e4b24aee68a680c6bc991ace7114a98
user-name-123-blip <aliberdimurat74@gmail.com>
2024-07-01 12:24:20 +0600

    Update blog.js

--- signal/login/lefsia/information/ali_blog/blog.js
+++ signal/login/lefsia/information/ali_blog/blog.js
@@ -1,1 +1,11 @@
+alert("Hello, Ali");
 
+function displayResult() {
+	document.getElementById("mynames").innerHTML = "Алиайдар Бердімұратов";
+}
+
+function funcDate() {
+	document.getElementById("date").innerHTML = Date();
+	document.getElementById("date").style.textAlign = "right";
+	document.getElementById("date").style.textShadow = "3px 2px 2px #87CEFA";
+}
